d4ac2209dcf216ae4fe64c52d4c85d9caaea2205
HK560 <lan2496846801@gmail.com>
2021-01-29 18:41:56 +0800

    修改了workers-sakurafrp.js

--- workers-sakurafrp.js
+++ workers-sakurafrp.js
@@ -2,17 +2,17 @@
 const github_base = "kasuganosoras/frp-blog";
 
 // 设置站点信息
-var default_title	 = "SakuraFrp Blog - 樱花内网穿透官方博客";					// 站点标题（显示在浏览器标题栏）
-var default_intitle	 = "SakuraFrp Blog";								// 站点名称（显示在首页）
-var default_description  = "欢迎访问 Sakura Frp 官方博客，本博客分享与 Frp 相关的技术以及记录一些日常。"; 	// 站点简介，有利于 SEO
-var site_domain		 = "blog.natfrp.org";								// 站点域名
-var site_subtitle	 = "樱花内网穿透官方博客";							// 站点副标题
+var default_title	 = "elbadaernU - HK560's Blog";					// 站点标题（显示在浏览器标题栏）
+var default_intitle	 = "elbadaernU";								// 站点名称（显示在首页）
+var default_description  = "个人cloudflare博客，随便记录些东西"; 	// 站点简介，有利于 SEO
+var site_domain		 = "blog.hk560.top";								// 站点域名
+var site_subtitle	 = "与光同尘";							// 站点副标题
 var site_favicon	 = "https://cdn.zerodream.net/gitea/img/favicon.png";				// 站点 Logo
 
 // 博主信息
-var owner_name = "Akkariin";									// 博主名字
+var owner_name = "HK560";									// 博主名字
 var owner_logo = "https://secure.gravatar.com/avatar/80962ca1ced98d0e679b2bc315d049f2?s=256"	// 博主头像
-var owner_desc = "鸽子王/咸鱼/phper，日常水贴摸鱼，佛系出租服务器";					// 博主简介
+var owner_desc = "新世纪佛系战士";					// 博主简介
 
 // 设置站点资源文件地址
 var css_bootstrap	 = "https://cdn.zerodream.net/css/bootstrap.min.css";		// Boostrap css 文件地址
